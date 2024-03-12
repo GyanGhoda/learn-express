22fd5373ee7f7b446c6313087a16b18cb6da1408
GyanGhoda <gyan.ghoda@outlook.com>
2024-03-12 15:59:54 -0400

    exercises

--- quiz/readUsers.js
+++ quiz/readUsers.js
@@ -1,2 +1,18 @@
 const express = require('express')
 const router = express.Router();
+
+router.get('/usernames', (req, res) => {
+    let usernames = req.users.map(function (user) {
+        return { id: user.id, username: user.username };
+    });
+    res.send(usernames);
+});
+
+router.get('/username/:name', (req, res) => {
+    let user = req.users.find(function (user) {
+        return user.username === req.params.name;
+    });
+    res.send(user);
+});
+
+modules.exports = router;
